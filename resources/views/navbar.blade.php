78cc32a913ce194810bbc26b8427514d37d59aef
MeesCode <m.brinkhuis@student.tudelft.nl>
2020-04-20 22:47:47 +0200

    favicon

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-md navbar-dark shadow-sm">
 
     <a class="navbar-brand cursor-pointer" id="opensidebar">
-        <i class="fa fa-bars"></i>
+        <i class="fa fa-bars mr-2"></i>
         {{ config('app.name') }}
     </a>
 
